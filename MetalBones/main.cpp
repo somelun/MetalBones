1d38cd84368e144629f7933d6a1c1a3928830c1d
Sasha <somelun@gmail.com>
2024-07-21 20:33:37 +0100

    Initial commit

---
+++ MetalBones/main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  MetalBones
+//
+//  Created by Sasha on 21/07/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    std::cout << "Hello, World!\n";
+    return 0;
+}
