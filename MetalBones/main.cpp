712de9933de605802aca56ead878161814f84b07
Sasha <somelun@gmail.com>
2024-07-21 21:26:45 +0100

    Use auto generated single header metal-cpp bridge file

--- MetalBones/main.cpp
+++ MetalBones/main.cpp
@@ -5,17 +5,12 @@
 //  Created by Sasha on 21/07/2024.
 //
 
-#include <iostream>
-
 #define NS_PRIVATE_IMPLEMENTATION
 #define CA_PRIVATE_IMPLEMENTATION
 #define MTL_PRIVATE_IMPLEMENTATION
 
-#include <Foundation/Foundation.hpp>
-#include <Metal/Metal.hpp>
-#include <QuartzCore/QuartzCore.hpp>
+#include "../ThirdParty/Metal.hpp"
 
 int main(int argc, const char * argv[]) {
-    std::cout << "Hello, World!\n";
     return 0;
 }
