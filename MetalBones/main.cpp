014958e1c0bc7f99d340552e10f6c3cba583b1d3
Sasha <somelun@gmail.com>
2024-07-21 21:20:09 +0100

    Remove metalc-cpp from the repository and use header search path instead

--- MetalBones/main.cpp
+++ MetalBones/main.cpp
@@ -7,6 +7,14 @@
 
 #include <iostream>
 
+#define NS_PRIVATE_IMPLEMENTATION
+#define CA_PRIVATE_IMPLEMENTATION
+#define MTL_PRIVATE_IMPLEMENTATION
+
+#include <Foundation/Foundation.hpp>
+#include <Metal/Metal.hpp>
+#include <QuartzCore/QuartzCore.hpp>
+
 int main(int argc, const char * argv[]) {
     std::cout << "Hello, World!\n";
     return 0;
